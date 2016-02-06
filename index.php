cddfd65c020f19cdbb5fd22d0a8416452225fc01
Sawers <acer.iconia.dak@gmail.com>
2016-02-06 15:20:49 +0000

    adding the index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index page</title>
+</head>
+<body>
+	<h1>Title of the page</h1>
+	<ul>
+	    <li>menu 1</li>
+	    <li>menu 2</li>
+	    <li>menu 3</li>
+	</ul>
+</body>
+</html>
